dc95f6b64c035e7fc7a0b421ced7c36ab7de6f48
Mondale Felix <mondalefelix@yahoo.com>
2018-03-31 22:26:23 -0700

    completed page 2

--- AppDevelopmentSwift4-Student/1 - Getting Started/4 - Control Flow/lab/Lab - Control Flow.playground/Pages/1. Exercise - Logical Operators.xcplaygroundpage/Contents.swift
+++ AppDevelopmentSwift4-Student/1 - Getting Started/4 - Control Flow/lab/Lab - Control Flow.playground/Pages/1. Exercise - Logical Operators.xcplaygroundpage/Contents.swift
@@ -10,46 +10,62 @@
  
  1. `9 == 9`
  */
-
-
+print(true);
+print(9 == 9);
+print("----");
 /*:
  2. `9 != 9`
  */
-
-
+print(false);
+print(9 != 9);
+print("----");
 /*: 
  3. `47 > 90`
  */
-
+print(false);
+print(47 > 90);
+print("----");
 
 /*:
  4. `47 < 90`
  */
-
+print(true);
+print(47 < 90);
+print("----");
 
 /*:
  5. `4 <= 4`
  */
-
+print(true);
+print(4 <= 4);
+print("----");
 
 /*:
  6. `4 >= 5`
  */
-
+print(false);
+print(4 >= 5);
+print("----");
 
 /*:
  7. `(47 > 90) && (47 < 90)`
  */
-
+print(false);
+print((47 > 90) && (47 < 90))
+print("----");
 
 /*:
  8. `(47 > 90) || (47 < 90)`
  */
-
+print(true);
+print((47 > 90) || (47 < 90))
+print("----");
 
 /*:
  9. `!true`
  */
+print(false);
+print(!true);
 
 
 //: page 1 of 9  |  [Next: Exercise - If and If-Else Statements](@next)
