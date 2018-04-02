7a48bb39a7d274e702a81861ff2b77baabad48db
Mondale Felix <mondalefelix@yahoo.com>
2018-04-02 09:10:14 -0700

    refactored

---
+++ AppDevelopmentSwift4-Student/1 - Getting Started/Light/Light/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Light
+//
+//  Created by Mac on 4/2/18.
+//  Copyright © 2018 Mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var lightOn = true;
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        lightOn = !lightOn
+        updateUI()
+    }
+
+    func updateUI(){
+        if lightOn {
+            view.backgroundColor = .white
+        } else {
+            view.backgroundColor = .black
+        }
+    }
+    
+}
+
