77e4bf45bcc6d0bb882b0be7cb0e7afde781e59c
Mondale Felix <mondalefelix@yahoo.com>
2018-05-28 15:58:02 -0700

    created spaceship base class

--- AppDevelopmentSwift4-Student/2 - Introduction to UIKit/3 - Structures/lab/Lab - Structures.playground/Pages/8. App Exercise - Mile Times and Congratulations.xcplaygroundpage/Contents.swift
+++ AppDevelopmentSwift4-Student/2 - Introduction to UIKit/3 - Structures/lab/Lab - Structures.playground/Pages/8. App Exercise - Mile Times and Congratulations.xcplaygroundpage/Contents.swift
@@ -26,11 +26,21 @@ print(firstWorkout.averageMileTime)
  In the `Steps` struct below, add a `willSet` to the `steps` property that will check if the new value is equal to `goal`, and if it is, prints a congratulatory message. Create an instance of `Steps` where `steps` is 9999 and `goal` is 10000, then call `takeStep()` and see if your message is printed to the console.
  */
 struct Steps {
-    var steps: Int
+    var steps: Int{
+        willSet{
+            if newValue == goal {
+                print("Congratulations you turd")
+            }
+        }
+    }
     var goal: Int
     
     mutating func takeStep() {
+        
         steps += 1
     }
 }
+
+var firstDay = Steps(steps: 9999, goal: 10000)
+firstDay.takeStep()
 //: [Previous](@previous)  |  page 8 of 10  |  [Next: Exercise - Type Properties and Methods](@next)
--- AppDevelopmentSwift4-Student/2 - Introduction to UIKit/3 - Structures/lab/Lab - Structures.playground/Pages/9. Exercise - Type Properties and Methods.xcplaygroundpage/Contents.swift
+++ AppDevelopmentSwift4-Student/2 - Introduction to UIKit/3 - Structures/lab/Lab - Structures.playground/Pages/9. Exercise - Type Properties and Methods.xcplaygroundpage/Contents.swift
@@ -7,12 +7,24 @@ struct User {
     var userName: String
     var email: String
     var age: Int
+    
+    static var currentUser = User(userName: "Mondale", email: "turd", age: 20)
+
+    mutating func logIn(user: User){
+        User.currentUser = user
+        print("\(User.currentUser.userName) has logged in")
+
+
+        
+    }
 }
-/*:
- There are other properties and actions associated with a `User` struct that might be good candidates for a type property or method. One might be a method for logging in. Go back and create a type method called `logIn(user:)` where `user` is of type `User`. In the body of the method, assign the passed in user to the `currentUser` property, and print out a statement using the user's userName saying that the user has logged in.
- 
- Below, call the `logIn(user:)` method and pass in a different `User` instance than what you assigned to currentUser above. Observe the printout in the console.
- */
+
+print(User.currentUser)
+
+var newUser = User(userName: "Mondale", email: "testing", age: 20)
+
+newUser.logIn(user: newUser)
+
 
 
 //: [Previous](@previous)  |  page 9 of 10  |  [Next: App Exercise - Type Properties and Methods](@next)
